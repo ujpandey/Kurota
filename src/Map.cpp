c617df0ed87f1550587e93698e01faf6b2a1e475
ujpandey <ujpandey1@cougars.ccis.edu>
2014-11-06 15:22:26 -0600

    asdfdf

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,1 +1,43 @@
 #include "Map.h"
+#include "Game.h"
+#include "TextureManager.h"
+
+//-----------------------------------------------------------------------------
+// TileSet
+//-----------------------------------------------------------------------------
+TileSet::TileSet(const std::string & id, const std::string & filename)
+    : _id(id)
+{
+    TextureManager * tm = TextureManager::get_instance();
+    tm -> load(filename, _id, Game::get_instance() -> get_renderer());
+}
+
+TileSet::~TileSet()
+{
+    TextureManger * tm = TextureManager::get_instance();
+    tm -> erase(_id);
+}
+
+const std::string & TileSet::get_id()
+{
+    return _id;
+}
+
+//-----------------------------------------------------------------------------
+// Tile
+//-----------------------------------------------------------------------------
+Tile::Tile(const std::string & id,
+           int x, int y, int w, int h)
+    : GameObject(id)
+{
+    _bounding_rect = {x, y, w, h};
+}
+
+const SDL_Rect * Tile::get_bounds() const
+{
+    retrun &_bounding_rect;
+}
+
+void Tile::draw()
+{
+}
--- src/Map.h
+++ src/Map.h
@@ -3,21 +3,37 @@
 
 #include "GameObject.h"
 
+enum TileType { GRASS, GROUND, PAVEMENT, VEGETATION, WATER };
+
+Class TileSet
+{
+public:
+    TileSet(const std::string & id, const std::string & filename);
+    ~TileSet();
+    const std::string & get_id() const;
+    
+private:
+    std::string _id;
+};
+
 Class Tile : public GameObject
 {
 public:
-    Tile(const std::string & id,
-}
+    Tile(const std::string & id, int num, int size,
+         int x, int y, int w, int h);
+    virtual const SDL_Rect * get_bounds() const;
+    virtual void draw();
+    virtual void update();
 
-Class Cell : public GameObject
-{
-public:
-    Cell(const int & x, const int & y, );
-    virtual ~Cell();
-
-
-private:
-    std::vector< Cell * > neighbors;
+    virtual const bool & empty() const;
+    virtual void release_occupant(GameObject * g_o);
+    virtual void set_occupant(GameObject * g_o);
+    
+protected:
+    bool _empty;
+    SDL_Rect _bounding_rect;
+    std::vector< Tile * > neighbors;
+    std::vector< GameObject * > occupants;
 };
 
 Class Map : public GameObject
